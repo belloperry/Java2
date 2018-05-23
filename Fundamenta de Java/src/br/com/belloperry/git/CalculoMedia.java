4243e89cfe5fee581ab5964348a92ff59532730b
bello.puwadiae <bello.puwadiae@TAT0541218W10-1.senacsp.edu.br>
2018-05-23 17:01:53 -0300

    Add class calculoMedia

---
+++ Fundamenta de Java/src/br/com/belloperry/git/CalculoMedia.java
@@ -0,0 +1,33 @@
+package br.com.belloperry.git;
+
+import java.util.Scanner;
+
+public class CalculoMedia {
+
+	public static void main(String[] args) {
+		// a linha abaixo cria variaveis do tipo real
+		float nota1, nota2, media;
+		//entrada
+		System.out.print("Digite a nota1: ");
+		// a linha abaixo cria um objeto de nome teclado usando o Scanner
+		Scanner teclado = new Scanner(System.in);
+		// a linha abaixo usa o objeto teclado para capturar o que foi
+		// digitado no console e armazenar na variável nota1
+		nota1 = teclado.nextFloat();
+		System.out.print("Digite a nota2: ");
+		nota2 = teclado.nextFloat();
+		// processamento
+		media = (nota1+ nota2) / 2;
+		// saida 
+		// + concatena(junta) um texto com o conteudo da variavel media
+		System.out.println("Media final" + media);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
