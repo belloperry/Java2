e7d9041c7f954a175b36dfe4b55915dc7ade9b70
bello.puwadiae <bello.puwadiae@TAT0541218W10-1.senacsp.edu.br>
2018-05-29 14:41:56 -0300

    Using String for name

---
+++ Fundamenta de Java/src/br/com/belloperry/git/aula4.java
@@ -0,0 +1,15 @@
+package br.com.belloperry.git;
+
+public class aula4 {
+
+	public static void main(String[] args) {
+		// class for name calcular
+		String nome = "Perry";
+		String sobrnome = "Uwadiae";
+		String idade = "32";
+		System.out.println(nome + " de " + sobrnome);
+		System.out.println(idade);
+
+	}
+
+}
