b6e745d4e064de0925751f5732ae26a993286d30
bello.puwadiae <bello.puwadiae@TAT0541218W10-1.senacsp.edu.br>
2018-06-11 16:06:04 -0300

    Addicional EstudoWhile

---
+++ Fundamenta de Java/src/br/com/belloperry/git/Estudowhile.java
@@ -0,0 +1,23 @@
+package br.com.belloperry.git;
+
+public class Estudowhile {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 1;
+		while (x < 10) {
+			System.out.println("teste while");
+			x++;
+		}
+		do {
+		   System.out.println("");
+		   System.out.println("teste do while");
+		   y++;
+		   System.out.println("teste do while");
+		}
+		while (y < 11);
+		
+
+	}
+
+}
