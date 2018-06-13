ea1112d0d750ecbc94d65523998ffe6c09381d1a
bello.puwadiae <bello.puwadiae@TAT0541218W10-1.senacsp.edu.br>
2018-06-13 17:15:17 -0300

    Calculator of IT

---
+++ Fundamenta de Java/src/br/com/belloperry/git/CalculadoraTi.java
@@ -0,0 +1,20 @@
+package br.com.belloperry.git;
+
+import java.util.Scanner;
+
+public class CalculadoraTi {
+
+	public static void main(String[] args) {
+		double remuneracao,cargo,custo;
+		System.out.println("Calculadora de TI");
+		Scanner teclado = new Scanner(System.in);
+		System.out.println(" ");
+		System.out.println("Precos de Remuneracao: ");
+		remuneracao = teclado.nextDouble();
+		System.out.println("% de cargo: ");
+		
+		
+
+	}
+
+}
