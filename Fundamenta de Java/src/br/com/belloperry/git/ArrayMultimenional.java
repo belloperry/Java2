95f1cee1e65b2142b026a8e169fda57afb33c85e
bello.puwadiae <bello.puwadiae@TAT0541218W10-1.senacsp.edu.br>
2018-06-18 17:24:19 -0300

    array Multi

---
+++ Fundamenta de Java/src/br/com/belloperry/git/ArrayMultimenional.java
@@ -0,0 +1,12 @@
+package br.com.belloperry.git;
+
+public class ArrayMultimenional {
+
+	public static void main(String[] args) {
+		// alinha abaixo cria um array multidimensional (metriz)
+		double [][] boletim  = {{8,7,9,3},{4,5,8,6}};
+		// recuperando a nota portugues
+		System.out.println("Media de portugues do 3 bim: " + boletim[1][2]);
+		}
+
+}
